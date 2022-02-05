e73b91b6975bfe711b305402352691a715034c63
Elrie <79701587+ElrieM@users.noreply.github.com>
2022-02-05 01:34:30 +0100

    feat: Add teaser part of header

--- blacksheep/blacksheep/settings.py
+++ blacksheep/blacksheep/settings.py
@@ -96,7 +96,7 @@ ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 6
 LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/p'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 # Google as the OAuth provider
@@ -160,7 +160,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- blacksheep/home/views.py
+++ blacksheep/home/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 def index(request):
     """ View to return index.html """
-    
+
     return render(request, 'home/index.html')

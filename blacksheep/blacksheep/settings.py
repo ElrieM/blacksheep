b1ae92142141d57b90aa81381e3f9b213fe1f6e3
Elrie <79701587+ElrieM@users.noreply.github.com>
2022-02-05 04:29:34 +0100

    fix: Resolve static file loading error and add favicons

--- blacksheep/blacksheep/settings.py
+++ blacksheep/blacksheep/settings.py
@@ -14,7 +14,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -121,7 +121,7 @@ WSGI_APPLICATION = 'blacksheep.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
     }
 }
 
@@ -153,6 +153,8 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
+
+USE_TZ = True
 
 USE_TZ = True
 

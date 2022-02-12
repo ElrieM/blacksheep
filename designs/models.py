84377aebed363d42ebce7d88bf7fa119afbb097a
Elrie <79701587+ElrieM@users.noreply.github.com>
2022-02-12 02:04:29 +0100

    docs: added design models and settings

--- designs/models.py
+++ designs/models.py
@@ -1,3 +1,41 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from contextlib import nullcontext
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+# Internal:
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+class Mockup(models.Model):
+    """
+    Mockup template model class
+    """
+
+    class Meta:
+        """
+            Plural name for class
+        """
+        verbose_name_plural = 'Mockups'
+
+    name = models.CharField(
+        verbose_name=_('Name'),
+        max_length=254
+    )
+    friendly_name = models.CharField(
+        max_length=254,
+        null=True,
+        blank=True
+    )
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    base_price = models.DecimalField(max_digits=6, decimal_places=2)
+    has_sizes = models.BooleanField(default=True, null=True, blank=True)
+
+    def __str__(self):
+        """
+        Returns category name string
+        """
+        return self.name

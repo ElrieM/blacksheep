011e969be2c2c788bd181c0f4aa2ab33c02e792e
Elrie <79701587+ElrieM@users.noreply.github.com>
2022-02-18 14:46:17 +0100

    docs: Added add-to-bag functionality

--- cart/urls.py
+++ cart/urls.py
@@ -1,6 +1,13 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.urls import path
+
+# Internal:
 from . import views
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 urlpatterns = [
     path('', views.view_cart, name="view_cart"),
+    path('add/<item_id>', views.add_to_cart, name="add_to_cart"),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from django.shortcuts import render, redirect
+
+# Internal:
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 def view_cart(request):
     """ View to renders products in the shopping cart """
 
     return render(request, 'cart/shopcart.html')
+
+
+def add_to_cart(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)

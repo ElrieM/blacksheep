0c783750b250f0b0919fe00dc27fec62e41af77f
Elrie <79701587+ElrieM@users.noreply.github.com>
2022-02-22 19:46:19 +0100

    docs: Added profile form, views and udpated template

---
+++ profiles/forms.py
@@ -0,0 +1,47 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
+from django import forms
+
+# Internal:
+from .models import UserProfile
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+class UserProfileForm(forms.ModelForm):
+    """
+    Order form class
+    """
+
+    class Meta:
+        """
+        Meta class information
+        """
+        model = UserProfile
+        exclude = ('user',
+        )
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_phone_number': 'Phone Number',
+            'default_street_address1': 'Street Address',
+            'default_street_address2': 'Street Address',
+            'default_town_or_city': 'Town or City',
+            'default_postcode': 'Post Code',
+            'default_county': 'County, State or Locality',
+        }
+
+        self.fields['default_phone_number'].widget.attrs['autofocus'] =  True
+        for field in self.fields:
+            if field != 'default_country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+                self.fields[field].widget.attrs['class'] = ('profile-form-input')
+            self.fields[field].label = False

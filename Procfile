c07eab279b99e9191e5dd5f92f8af50c0fc1e5eb
Elrie <79701587+ElrieM@users.noreply.github.com>
2022-02-24 00:58:30 +0100

    docs: Deploy to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn blacksheep.wsgi:application
--- products/models.py
+++ products/models.py
@@ -4,7 +4,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-# Internal:
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 class Category(models.Model):

ebe93e2aa3cbb1a7956d7fd85f25b87eac732ca7
Elrie <79701587+ElrieM@users.noreply.github.com>
2022-02-24 16:55:30 +0100

    Revert "docs: Deploy to Heroku"
    This reverts commit c07eab279b99e9191e5dd5f92f8af50c0fc1e5eb.

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn blacksheep.wsgi:application
--- products/models.py
+++ products/models.py
@@ -4,6 +4,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+# Internal:
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 class Category(models.Model):

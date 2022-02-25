0e117e5a68332cd7219750101d81fbadfb5a2ac3
Elrie <79701587+ElrieM@users.noreply.github.com>
2022-02-25 10:28:05 +0100

    fix: error with css mime type mismatch

--- blacksheep/settings.py
+++ blacksheep/settings.py
@@ -187,12 +187,14 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

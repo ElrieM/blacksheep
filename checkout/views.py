e9af5d52002afe35c8f03366523bae4d11392251
Elrie <79701587+ElrieM@users.noreply.github.com>
2022-04-01 19:50:59 +0100

    fix: Resolve order details not showing on checkout success page and formatting code in checkout app

--- checkout/views.py
+++ checkout/views.py
@@ -55,25 +55,25 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    if request.method == "POST":
-        cart = request.session.get("cart", {})
+    if request.method == 'POST':
+        cart = request.session.get('cart', {})
 
         form_data = {
-            "full_name": request.POST["full_name"],
-            "email": request.POST["email"],
-            "phone_number": request.POST["phone_number"],
-            "country": request.POST["country"],
-            "postcode": request.POST["postcode"],
-            "town_or_city": request.POST["town_or_city"],
-            "street_address1": request.POST["street_address1"],
-            "street_address2": request.POST["street_address2"],
-            "county": request.POST["county"],
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
         }
 
         order_form = OrderForm(form_data)
         if order_form.is_valid():
             order = order_form.save(commit=False)
-            pid = request.POST.get("client_secret").split("_secret")[0]
+            pid = request.POST.get('client_secret').split('_secret')[0]
             order.stripe_pid = pid
             order.original_cart = json.dumps(cart)
             order.save()
@@ -97,30 +97,25 @@ def checkout(request):
                             )
                             order_line_item.save()
                 except Product.DoesNotExist:
-                    messages.error(
-                        request,
-                        (
-                            "One of the products in your cart was not found in the database. "
-                            "Please call us for assistance!"
-                        ),
+                    messages.error(request, (
+                        "One of the products in your cart was not found in the database. "
+                        "Please call us for assistance!"),
                     )
                     order.delete()
-                    return redirect(reverse("view_cart"))
+                    return redirect(reverse('view_cart'))
 
-            request.session["save_info"] = "save_info" in request.POST
-            return redirect(reverse("checkout_success", args=[order.order_number]))
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
-            messages.error(
-                request, "An error occurred, please double-check your details."
-            )
+            messages.error(request, "An error occurred, please double-check your details.")
     else:
-        cart = request.session.get("cart", {})
+        cart = request.session.get('cart', {})
         if not cart:
             messages.error(request, "Cart is empty!")
-            return redirect(reverse("products"))
+            return redirect(reverse('products'))
 
         current_cart = cart_contents(request)
-        total = current_cart["grand_total"]
+        total = current_cart['grand_total']
         stripe_total = round(total * 100)
         stripe.api_key = stripe_secret_key
         intent = stripe.PaymentIntent.create(
@@ -134,15 +129,15 @@ def checkout(request):
                 profile = UserProfile.objects.get(user=request.user)
                 order_form = OrderForm(
                     initial={
-                        "full_name": profile.user.get_full_name(),
-                        "email": profile.user.email,
-                        "phone_number": profile.default_phone_number,
-                        "country": profile.default_country,
-                        "postcode": profile.default_postcode,
-                        "town_or_city": profile.default_town_or_city,
-                        "street_address1": profile.default_street_address1,
-                        "street_address2": profile.default_street_address2,
-                        "county": profile.default_county,
+                        'full_name': profile.user.get_full_name(),
+                        'email': profile.user.email,
+                        'phone_number': profile.default_phone_number,
+                        'country': profile.default_country,
+                        'postcode': profile.default_postcode,
+                        'town_or_city': profile.default_town_or_city,
+                        'street_address1': profile.default_street_address1,
+                        'street_address2': profile.default_street_address2,
+                        'county': profile.default_county,
                     }
                 )
             except UserProfile.DoesNotExist:
@@ -151,12 +146,10 @@ def checkout(request):
             order_form = OrderForm()
 
     if not stripe_public_key:
-        messages.warning(
-            request,
-            "Stripe public key is missing. Did you forget to set it in your environment?",
-        )
+        messages.warning(request, "Stripe public key is missing. \
+                Did you forget to set it in your environment?")
 
-    template = "checkout/checkout.html"
+    template = 'checkout/checkout.html'
 
     context = {
         "order_form": order_form,
@@ -195,9 +188,7 @@ def checkout_success(request, order_number):
             if user_profile_form.is_valid():
                 user_profile_form.save()
 
-    messages.success(
-        request,
-        f'Order successfully completed.\
+    messages.success(request, f'Order successfully completed.\
         Your order number is {order_number}. A confirmation \
         email will be sent {order.email} shortly.',
     )
@@ -207,7 +198,7 @@ def checkout_success(request, order_number):
 
     template = 'checkout/checkout_success.html'
     context = {
-        order: order,
+        'order': order,
     }
 
     return render(request, template, context)
